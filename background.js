901c9d4d96a6d2c67db12e0de19016a88524a985
Olivier de Broqueville <odebroqueville@users.noreply.github.com>
2017-12-14 20:04:10 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,17 @@
 /// Initialisation
 function init() {
-	browser.storage.sync.set({"dummy-key": "dummy-data"}).then(function(){
-        browser.storage.sync.remove("dummy-key").then(function(){
-            browser.storage.sync.get(null).then(onGot, onError);
-        }, onError);
-    }, onError);
-
-    // Load search engines if they're not already loaded in storage sync
-	function onGot(data){
-        console.log("success");
-	}
-
-	function onError(error){
-		console.log("fail");
-        console.error(error);
-	}
+	browser.storage.sync.get(null).then(onGot, onError);
 }
+
+// Load search engines if they're not already loaded in storage sync
+function onGot(data){
+	console.log("success");
+	console.log(data);
+}
+
+function onError(error){
+	console.log("fail");
+        console.error(error);
+}
+
 init();
